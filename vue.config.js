12b97bed4af7486d5f4cd058e14423aec7d9166d
zhongjunwei <liwenxin@wisight.cn>
2022-07-18 16:07:37 +0800

    update github config

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ const { defineConfig } = require('@vue/cli-service')
 // }
 
 module.exports = defineConfig({
-  publicPath: '/vue-ad/', // 署应用包时的基本 URL。 vue-router hash 模式使用
+  publicPath: '/vue-beautiful-chat/', // 署应用包时的基本 URL。 vue-router hash 模式使用
   //  publicPath: '/app/', //署应用包时的基本 URL。  vue-router history模式使用
   outputDir: 'dist', //  生产环境构建文件的目录
   assetsDir: 'static', //  outputDir的静态资源(js、css、img、fonts)目录
