041b890bb1d7ba75de8c02fec43bbc97cbc5a48d
zhongjunwei <liwenxin@wisight.cn>
2022-07-18 17:50:29 +0800

    update github config

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ Vue.use(Router)
 
 const createRouter = () =>
   new Router({
-    // mode: 'history', // 如果你是 history模式 需要配置vue.config.js publicPath
+    mode: 'history', // 如果你是 history模式 需要配置vue.config.js publicPath
     // base: process.env.BASE_URL,
     scrollBehavior: () => ({ y: 0 }),
     routes: constantRouterMap
